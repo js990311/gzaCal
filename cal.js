bc0411c56c7263de943651bc400812afb4c162bf
identity230c <identity230c@kookmin.ac.kr>
2022-03-21 16:10:56 +0900

    change result

--- cal.js
+++ cal.js
@@ -10,13 +10,27 @@ $('button').on('click', function(){
   printer = document.getElementById('zdg-ret');
   printer.innerHTML = Math.round(zdg);
   $('td').removeClass('table-info');
+  $('#gza-box').removeClass('alert-danger');
+  $('#gza-box').removeClass('alert-warning');
+  $('#gza-box').removeClass('alert-primary');
+  $('#gza-box').addClass('alert-primary');
+  $('#zdg-box').removeClass('alert-danger');
+  $('#zdg-box').removeClass('alert-warning');
+  $('#zdg-box').removeClass('alert-primary');
+  $('#zdg-box').addClass('alert-primary');
 
   if(tmp >= 3.435){
     $('tr')[6].childNodes[3].classList.add('table-info');
+    $('#gza-box').removeClass('alert-primary');
+    $('#gza-box').addClass('alert-danger');
   }else if(tmp >= 1){
     $('tr')[5].childNodes[3].classList.add('table-info');
+    $('#gza-box').removeClass('alert-primary');
+    $('#gza-box').addClass('alert-danger');
   }else if(tmp>=0.233){
     $('tr')[4].childNodes[3].classList.add('table-info');
+    $('#gza-box').removeClass('alert-primary');
+    $('#gza-box').addClass('alert-warning');
   }else if(tmp>=0.1){
     $('tr')[3].childNodes[3].classList.add('table-info');
   }else if(tmp>=0.05){
@@ -27,11 +41,15 @@ $('button').on('click', function(){
 
   if(zdg >= 7933){
     $('tr')[6].childNodes[5].classList.add('table-info');
+    $('#zdg-box').removeClass('alert-primary');
+    $('#zdg-box').addClass('alert-danger');
   }else if(zdg >= 3333){
     $('tr')[5].childNodes[5].classList.add('table-info');
+    $('#zdg-box').removeClass('alert-primary');
     $('#zdg-box').addClass('alert-danger');
   }else if(zdg>=667){
     $('tr')[4].childNodes[5].classList.add('table-info');
+    $('#zdg-box').removeClass('alert-primary');
     $('#zdg-box').addClass('alert-warning');
   }else if(zdg>=333){
     $('tr')[3].childNodes[5].classList.add('table-info');
